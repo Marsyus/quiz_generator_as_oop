69afba4deacc5fe7fafe9b774e609dd55323fa82
Marsyus <miadolomboy0917@gmail.com>
2025-05-26 01:58:11 +0800

    Created file for the quiz creator along with its pseudocode

---
+++ quiz_creator_oop.py
@@ -0,0 +1,5 @@
+#Import the question class
+#Create a function that asks user input to generate a question
+#Create a function that writes the question in a text file
+#Create a function that asks user to continue or not
+#Call the functions to run the program
