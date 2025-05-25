6cf4a99fab81efc82f2104f26ceaae5c0f0c633c
Marsyus <miadolomboy0917@gmail.com>
2025-05-26 01:59:55 +0800

    Imported the question class

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -1,4 +1,5 @@
 #Import the question class
+from question_class import Question
 #Create a function that asks user input to generate a question
 #Create a function that writes the question in a text file
 #Create a function that asks user to continue or not
