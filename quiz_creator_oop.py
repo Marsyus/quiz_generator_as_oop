415c4ff215bc6d9df38b8855ff88a66a4b5dd411
Marsyus <miadolomboy0917@gmail.com>
2025-05-26 02:22:35 +0800

    Inherited the question class and added constructors for choices a to d

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -1,6 +1,12 @@
 #Import the question class
 from question_class import Question
 #Create a function that asks user input to generate a question
+class NewQuestion(Question):
+    def __init__(self, choice_a, choice_b, choice_c, choice_d):
+        self.choice_a = choice_a
+        self.choice_b = choice_b
+        self.choice_c = choice_c
+        self.choice_d = choice_d
 #Create a function that writes the question in a text file
 #Create a function that asks user to continue or not
 #Call the functions to run the program
