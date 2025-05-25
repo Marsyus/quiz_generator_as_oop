a57ad6e8d75617aa681271cfdc79c99f6acca33c
Marsyus <miadolomboy0917@gmail.com>
2025-05-26 02:30:43 +0800

    A direct implementation of my last code for quiz creator. Asks the user to input for question, choices, and answer

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -7,6 +7,24 @@ class NewQuestion(Question):
         self.choice_b = choice_b
         self.choice_c = choice_c
         self.choice_d = choice_d
+
+    def make_question(self):
+        self.question = "Question: "
+        self.question += input(f"Enter your question: ")
+        self.choice_a, self.choice_b, self.choice_c, self.choice_d = "a.) ", "b.) ", "c.) ", "d.) "
+        self.choice_a += input(f"Enter choice for letter a: ")
+        self.choice_b += input(f"Enter choice for letter b: ")
+        self.choice_c += input(f"Enter choice for letter c: ")
+        self.choice_d += input(f"Enter choice for letter d: ")
+        self.choices = ["a", "b", "c", "d"]
+        while True:
+            ans = input("Choose a correct answer [a/b/c/d]: ").lower()
+            if ans in self.choices:
+                self.answer = "Answer: " + ans
+                break
+            else:
+                print("Invalid input. Please try again.")
+
 #Create a function that writes the question in a text file
 #Create a function that asks user to continue or not
 #Call the functions to run the program
