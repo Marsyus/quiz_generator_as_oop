ea34f215b8a8452d95eba43c84cfcbc0d1b12805
Marsyus <miadolomboy0917@gmail.com>
2025-05-26 08:17:36 +0800

    Removed entire __init__ block and moved the code from the child class into the parent class

--- question_class.py
+++ question_class.py
@@ -1,6 +1,31 @@
 #Question Class
 class Question:
-    def __init__(self, question, choices, answer):
-        self.question = question
-        self.choices = choices
-        self.answer = answer
+    #Create a function that asks user input to generate a question
+    def make_question(self):
+        self.question = "Question: "
+        self.question += input(f"Enter your question: ")
+        self.choice_a, self.choice_b, self.choice_c, self.choice_d = "a.) ", "b.) ", "c.) ", "d.) "
+        self.choice_a += input(f"Enter choice for letter a: ")
+        self.choice_b += input(f"Enter choice for letter b: ")
+        self.choice_c += input(f"Enter choice for letter c: ")
+        self.choice_d += input(f"Enter choice for letter d: ")
+        self.valid_choices = ["a", "b", "c", "d"]
+        while True:
+            ans = input("Choose a correct answer [a/b/c/d]: ").lower()
+            if ans in self.valid_choices:
+                self.answer = "Answer: " + ans
+                break
+            else:
+                print("Invalid input. Please try again.")
+
+    def compile_choices(self):
+        self.choices = [self.choice_a, self.choice_b, self.choice_c, self.choice_d]    
+    
+    #Create a function that writes the question in a text file
+    def make_file(self, filename="quiz.txt"):
+        with open(filename, "a") as file:
+            file.write(f"{self.question}\n")
+            for choice in self.choices:
+                file.write(f"{choice}\n")
+            file.write(f"{self.answer}\n")
+            
--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -1,41 +1,9 @@
 #Import the question class
 from question_class import Question
-#Create a function that asks user input to generate a question
+
 class NewQuestion(Question):
-    def __init__(self, choice_a, choice_b, choice_c, choice_d):
-        self.choice_a = choice_a
-        self.choice_b = choice_b
-        self.choice_c = choice_c
-        self.choice_d = choice_d
+    ...
 
-    def make_question(self):
-        self.question = "Question: "
-        self.question += input(f"Enter your question: ")
-        self.choice_a, self.choice_b, self.choice_c, self.choice_d = "a.) ", "b.) ", "c.) ", "d.) "
-        self.choice_a += input(f"Enter choice for letter a: ")
-        self.choice_b += input(f"Enter choice for letter b: ")
-        self.choice_c += input(f"Enter choice for letter c: ")
-        self.choice_d += input(f"Enter choice for letter d: ")
-        self.valid_choices = ["a", "b", "c", "d"]
-        while True:
-            ans = input("Choose a correct answer [a/b/c/d]: ").lower()
-            if ans in self.valid_choices:
-                self.answer = "Answer: " + ans
-                break
-            else:
-                print("Invalid input. Please try again.")
-
-    def compile_choices(self):
-        self.choices = [self.choice_a, self.choice_b, self.choice_c, self.choice_d]    
-    
-    #Create a function that writes the question in a text file
-    def make_file(self, filename="quiz.txt"):
-        with open(filename, "a") as file:
-            file.write(f"{self.question}\n")
-            for choice in self.choices:
-                file.write(f"{choice}\n")
-            file.write(f"{self.answer}\n")
-            
 #Create a function that asks user to continue or not
 def ask_continue():
     while True:
